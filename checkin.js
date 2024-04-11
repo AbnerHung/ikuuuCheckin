f97f243684e273a28010c81eb9e0d79789287a90
AbnerHung <AbnerHung@foxmail.com>
2024-04-12 00:54:56 +0900

    Update checkin.js; Add login to get cookie;

--- checkin.js
+++ checkin.js
@@ -1,14 +1,28 @@
 const axios = require("axios");
 
-const NAME = process.env.NAME;
+const HOST = process.env.HOST;
+const EMAIL = process.env.EMAIL;
+const PASSWD = process.env.PASSWD;
 const SENDKEY = process.env.SENDKEY;
 
-axios.defaults.headers.common.cookie = process.env.COOKIE;
+const loginAndGetCookie = async () => {
+    const response = await axios({
+        method: 'post',
+        url: 'https://'+HOST+'/auth/login',
+        data: {
+            host: HOST,
+            email: EMAIL,
+            passwd: PASSWD
+        }
+    });
+    const cookie = response.headers['set-cookie']; // 获取响应中的cookie
+    axios.defaults.headers.common.cookie = cookie; // 将获取到的cookie设置为axios的默认header之一
+};
 
 const checkIn = async () => {
     return axios({
         method: 'post',
-        url: 'https://'+NAME+'/user/checkin'
+        url: 'https://'+HOST+'/user/checkin'
     })
 }
 
@@ -24,6 +38,7 @@ const server = (status, msg) => {
 }
 
 const ikuuuCheckin = async () => {
+    loginAndGetCookie();
     const checkinData = (await checkIn())?.data;
     const status = checkinData.ret
     const msg = checkinData.msg
